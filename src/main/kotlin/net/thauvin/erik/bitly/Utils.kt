708869deb1d3b64691d9c0651c6a28bbd563bbf2
Erik C. Thauvin <erik@thauvin.net>
2023-01-29 14:38:44 -0800

    Updated copyright

--- src/main/kotlin/net/thauvin/erik/bitly/Utils.kt
+++ src/main/kotlin/net/thauvin/erik/bitly/Utils.kt
@@ -1,8 +1,7 @@
 /*
  * Utils.kt
  *
- * Copyright (c) 2020-2022, Erik C. Thauvin (erik@thauvin.net)
- * All rights reserved.
+ * Copyright 2020-2023 Erik C. Thauvin (erik@thauvin.net)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
