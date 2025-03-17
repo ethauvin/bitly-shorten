feb7c73505f7dd0d94032639d6bdb7c337ab8a24
Erik C. Thauvin <erik@thauvin.net>
2025-03-16 21:32:49 -0700

    Update copyright

--- src/main/kotlin/net/thauvin/erik/bitly/Utils.kt
+++ src/main/kotlin/net/thauvin/erik/bitly/Utils.kt
@@ -1,7 +1,7 @@
 /*
  * Utils.kt
  *
- * Copyright 2020-2024 Erik C. Thauvin (erik@thauvin.net)
+ * Copyright 2020-2025 Erik C. Thauvin (erik@thauvin.net)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
--- src/main/kotlin/net/thauvin/erik/bitly/config/CreateConfig.kt
+++ src/main/kotlin/net/thauvin/erik/bitly/config/CreateConfig.kt
@@ -1,7 +1,7 @@
 /*
  * CreateConfig.kt
  *
- * Copyright 2020-2024 Erik C. Thauvin (erik@thauvin.net)
+ * Copyright 2020-2025 Erik C. Thauvin (erik@thauvin.net)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
