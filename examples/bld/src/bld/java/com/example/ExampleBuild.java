82b45b0bc15c135c8bdba68efbadef7b76e49b92
Erik C. Thauvin <erik@thauvin.net>
2025-06-30 16:29:15 -0700

    Revert to version 2.0.0 in the example until the new Maven Central API is implemented in bld

--- examples/bld/src/bld/java/com/example/ExampleBuild.java
+++ examples/bld/src/bld/java/com/example/ExampleBuild.java
@@ -24,7 +24,7 @@ public class ExampleBuild extends BaseProject {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, SONATYPE_SNAPSHOTS_LEGACY);
 
         scope(compile)
-                .include(dependency("net.thauvin.erik:bitly-shorten:2.0.1-SNAPSHOT"))
+                .include(dependency("net.thauvin.erik:bitly-shorten:2.0.0"))
                 .include(dependency("org.json:json:20250517"));
     }
 
