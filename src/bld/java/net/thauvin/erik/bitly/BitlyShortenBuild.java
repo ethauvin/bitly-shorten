2d13bb63b70a8348aa61525fa06a63f2dc991e5e
Erik C. Thauvin <erik@thauvin.net>
2025-08-14 20:44:46 -0700

    Bump Kotlin to version 2.2.10

--- src/bld/java/net/thauvin/erik/bitly/BitlyShortenBuild.java
+++ src/bld/java/net/thauvin/erik/bitly/BitlyShortenBuild.java
@@ -74,7 +74,7 @@ public class BitlyShortenBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL);
 
         var okHttp = version(5, 1, 0);
-        final var kotlin = version(2, 2, 0);
+        final var kotlin = version(2, 2, 10);
         scope(compile)
                 // Kotlin
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
